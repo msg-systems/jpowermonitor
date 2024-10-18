5d50bef42ba73a9b611f36f17f346b6ab57c6958
Jakob Deiner <jakob.deiner@msg.group>
2024-10-18 11:11:55 +0200

    Implemented fix from S. Schnell if total energy consumption is zero

--- src/main/java/group/msg/jpowermonitor/agent/export/statistics/StatisticsWriter.java
+++ src/main/java/group/msg/jpowermonitor/agent/export/statistics/StatisticsWriter.java
@@ -5,7 +5,6 @@ import group.msg.jpowermonitor.agent.PowerMeasurementCollector;
 import group.msg.jpowermonitor.agent.export.csv.CsvResultsWriter;
 import lombok.extern.slf4j.Slf4j;
 
-import java.text.NumberFormat;
 import java.util.Locale;
 
 import static group.msg.jpowermonitor.util.Constants.SEPARATOR;
@@ -23,23 +22,26 @@ public class StatisticsWriter {
 
     public void writeStatistics(CsvResultsWriter csvResultsWriter) {
         if (powerMeasurementCollector == null
-            || powerMeasurementCollector.getEnergyConsumptionTotalInJoule() == null
-            || powerMeasurementCollector.getEnergyConsumptionTotalInJoule().get() == null
-            || powerMeasurementCollector.getEnergyConsumptionTotalInJoule().get().getValue() == null) {
+                || powerMeasurementCollector.getEnergyConsumptionTotalInJoule() == null
+                || powerMeasurementCollector.getEnergyConsumptionTotalInJoule().get() == null
+                || powerMeasurementCollector.getEnergyConsumptionTotalInJoule().get().getValue() == null) {
             return;
         }
         String appStatistics = String.format("Application consumed %.2f joule - %.3f wh - %.6f kwh - %.3f gCO2 total",
-            powerMeasurementCollector.getEnergyConsumptionTotalInJoule().get().getValue(),
-            convertJouleToWattHours(powerMeasurementCollector.getEnergyConsumptionTotalInJoule().get().getValue()),
-            convertJouleToKiloWattHours(powerMeasurementCollector.getEnergyConsumptionTotalInJoule().get().getValue()),
-            powerMeasurementCollector.getEnergyConsumptionTotalInJoule().get().getCo2Value());
-        String benchmarkResult = hasBenchmarkResult() ?
-            "Benchmark result efficiency factor (sum of all loop counters / energyConsumptionTotal): *** "
-            + NumberFormat.getNumberInstance(Locale.GERMANY)
-                .format(getBenchmarkResult() / powerMeasurementCollector.getEnergyConsumptionTotalInJoule().get().getValue().longValue())
-            + " *** jPMarks" : "";
-        String filesInfo = "Energy consumption per method written to '" + csvResultsWriter.getEnergyConsumptionPerMethodFileName()
-                           + "' and filtered methods written to '" + csvResultsWriter.getEnergyConsumptionPerFilteredMethodFileName() + "'" + "\n" + SEPARATOR;
+                powerMeasurementCollector.getEnergyConsumptionTotalInJoule().get().getValue(),
+                convertJouleToWattHours(powerMeasurementCollector.getEnergyConsumptionTotalInJoule().get().getValue()),
+                convertJouleToKiloWattHours(powerMeasurementCollector.getEnergyConsumptionTotalInJoule().get().getValue()),
+                powerMeasurementCollector.getEnergyConsumptionTotalInJoule().get().getCo2Value());
+        long totalEnergyConsumptionInJoule = powerMeasurementCollector.getEnergyConsumptionTotalInJoule().get()
+                .getValue().longValue();
+        String benchmarkResult = hasBenchmarkResult() && totalEnergyConsumptionInJoule > 0.0 ? String.format(
+                Locale.GERMANY,
+                "Benchmark result efficiency factor (sum of all loop counters / energyConsumptionTotal): *** %,d *** jPMarks",
+                getBenchmarkResult() / totalEnergyConsumptionInJoule) : "";
+        String filesInfo = "Energy consumption per method written to '"
+                + csvResultsWriter.getEnergyConsumptionPerMethodFileName()
+                + "' and filtered methods written to '"
+                + csvResultsWriter.getEnergyConsumptionPerFilteredMethodFileName() + "'" + "\n" + SEPARATOR;
 
         if (JPowerMonitorAgent.isSlf4jLoggerImplPresent()) {
             log.info(appStatistics);
